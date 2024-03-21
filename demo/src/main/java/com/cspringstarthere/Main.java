8ba9f2b196ff8d87d6a241a22dd004bf5c517d13
Caleb <caleblyx@outlook.com>
2024-03-21 22:00:29 +0800

    initialize maven project and create basic skeleton

---
+++ demo/src/main/java/com/cspringstarthere/Main.java
@@ -0,0 +1,9 @@
+package com.cspringstarthere;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext();
+    }
+}
