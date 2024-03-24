cb5bbccc09f02eb5a3e4b5522ae664f79dd2270a
Caleb <caleblyx@outlook.com>
2024-03-24 19:29:43 +0800

    complete ex7

--- demo/src/main/java/com/cspringstarthere/main/Parrot.java
+++ demo/src/main/java/com/cspringstarthere/main/Parrot.java
@@ -1,10 +1,17 @@
 package com.cspringstarthere.main;
 
 import org.springframework.stereotype.Component;
+import javax.annotation.PostConstruct;
 
 @Component
 public class Parrot {
     private String name;
+    
+    @PostConstruct
+    public void init() {
+        this.name = "Kiki";
+    }
+    
     public String getName() {
         return name;
     }
