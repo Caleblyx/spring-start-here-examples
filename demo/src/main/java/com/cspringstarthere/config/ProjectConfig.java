753355c8f45ccff11328605aa8a9d03c22e1cb82
Caleb <caleblyx@outlook.com>
2024-03-28 10:04:54 +0800

    remove @ComponentScan annotation

--- demo/src/main/java/com/cspringstarthere/config/ProjectConfig.java
+++ demo/src/main/java/com/cspringstarthere/config/ProjectConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
-@ComponentScan(basePackages = "com.cspringstarthere.beans")
 @Configuration
 public class ProjectConfig {
     
