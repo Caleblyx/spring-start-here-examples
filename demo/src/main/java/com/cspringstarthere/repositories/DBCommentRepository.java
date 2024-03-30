2f51bfefa91c32842057ae24021a784750a36960
Caleb <caleblyx@outlook.com>
2024-03-30 18:20:31 +0800

    different service use different prototypes

--- demo/src/main/java/com/cspringstarthere/repositories/DBCommentRepository.java
+++ demo/src/main/java/com/cspringstarthere/repositories/DBCommentRepository.java
@@ -1,11 +1,14 @@
 package com.cspringstarthere.repositories;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import com.cspringstarthere.model.Comment;
 
 @Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class DBCommentRepository implements CommentRepository {
     
     @Override
